74605cc02d6b200d115e2d374f4fdc0798054e6d
Osama Haque <haqueosama1@gmail.com>
2022-12-01 19:25:12 +0600

    LED codes added in 'hc 12 receiver' file

--- hc12_receiver/hc12_receiver.ino
+++ hc12_receiver/hc12_receiver.ino
@@ -12,21 +12,31 @@
 #define TX 3                      //Connect to the RX pin of the HC-12
 //#define RX 10                   //Connect to the TX pin of the HC-12
 //#define TX 11                   //Connect to the RX pin of the HC-12
+#define yellowLED 11
+#define redLED 12
+#define pushButton 6
 
 //#define SD_ChipSelectPin 53     // example uses hardware SS pin 53 on Mega2560
-//#define SD_ChipSelectPin 10       // using digital pin 10 on arduino uno 328, can use other pins
-//#define SD_SpeakerPin 9        
+#define SD_ChipSelectPin 10       // using digital pin 10 on arduino uno 328, can use other pins
+#define SD_SpeakerPin 9        
 
 SoftwareSerial mySerial(RX, TX);
 TMRpcm tmrpcm;                    // needed for sd card module 
-
+String found = "";
 
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
 
+  pinMode(yellowLED, OUTPUT);     // setting the pins as digital output pins (working as alert LED's)
+  pinMode(redLED, OUTPUT);
+  pinMode(pushButton, INPUT);     // setting pushButton pin as input (when owner pushes a button, allert LED's are turned off
+
+  digitalWrite(yellowLED, LOW);   // at first there is no alert, hence setting their digital pins low
+  digitalWrite(redLED, LOW);
+
   //sd card setup
-  //tmrpcm.speakerPin = SD_SpeakerPin;   //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
+  tmrpcm.speakerPin = SD_SpeakerPin;   //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
 //  if(!SD.begin(SD_ChipSelectPin)){
 //    Serial.println("SD fail");
 //    return;
@@ -36,20 +46,42 @@ void setup() {
 
 }
 
-//void playAudio();
+void playAudio();
+void turnOffLED();
 
 
 void loop() { // run over and over
   if (mySerial.available()) {
+    found = mySerial.readString();
     Serial.write(mySerial.read());
+
+    if(found.indexOf("VIBRATION DETECTED") > -1){
+      digitalWrite(yellowLED, HIGH);              // vibration detected, turn yellow led on
+      
+    }else if(found.indexOf("LASER COMPROMISED") > -1){
+      digitalWrite(redLED, HIGH);                 // laser is compromised, turn red led on
+    }
+    found = "";
+    
   }
   if (Serial.available()) {
     mySerial.write(Serial.read());
   }
+
+
+  turnOffLED();
 }
 
-//// play sd card audio 
-//void playAudio(){
-//  tmrpcm.setVolume(6);
-//  tmrpcm.play("song.wav");
-//}
+// play sd card audio 
+void playAudio(){
+  tmrpcm.setVolume(6);
+  tmrpcm.play("song.wav");
+}
+
+
+void turnOffLED(){  
+  if(digitalRead(pushButton) == HIGH){          // turning LED's off when button pushed    
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(redLED, LOW);  
+  }
+}
